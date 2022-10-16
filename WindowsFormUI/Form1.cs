27fd92d177f83a74bbfe16fbeb20473bfa280f44
ARIBILGI <ARIBILGI@BLGAE>
2022-10-16 14:01:18 +0300

    Add project files.

---
+++ WindowsFormUI/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormUI
+{
+    //Form: WindowsForms altyapısının tüm ekranlarının geldiği temel (base) sınıftır.
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            //Form tasarımının ve üzerindeki tüm elementlerin/komponentlerin tanımlanması için işletilir.
+            InitializeComponent();
+        }
+
+        //Button (button1) elementi için click olayı oluşturuldu.
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Label (label1) elementinin içindeki Text alanı değiştiril.
+            //label1.Text = "Merhaba dünya!!!";
+            label1.Text = textBox1.Text;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Para para = new Para();
+            int sonuc = para.At();
+
+            if (sonuc == 1)
+            {
+                label2.Text = "Yazı";
+            }
+            else if(sonuc==2)
+            {
+                label2.Text = "Tura";
+            }
+            else
+            {
+                label2.Text = "Dik";
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Tavla tavla = new Tavla();
+            string sonuc = tavla.At() + " + " + tavla.At();
+            label2.Text = sonuc;
+        }
+    }
+}
---
+++ WindowsFormUI/Zar.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormUI
+{
+    public interface IZar
+    {
+        int At();
+    }
+    public class Zar : IZar
+    {
+        private int yuz_sayisi;
+        private static readonly Random r = new Random();
+        public Zar(int yuz_sayisi)
+        {
+            this.yuz_sayisi = yuz_sayisi;
+        }
+        public int At()
+        {
+            return r.Next(yuz_sayisi) + 1;
+        }
+    }
+    //base: Kalıtımda temel(base) sınıfın yapıcı metodu parametre istiyorsa :base(parametre) yapısı ile gönderilir.
+    public class Para : Zar
+    {
+        public Para() : base(2) { }
+    }
+
+    public class Tavla : Zar
+    {
+        public Tavla() : base(6) { }
+    }
+}
